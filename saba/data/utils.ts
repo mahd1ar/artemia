13dcd362fb56fa4f7bed9cd1b89154a6a2e49d5d
mahdiyar anari <mahdiyaranari@yahoo.com>
2024-03-14 11:43:53 +0330

    add code and estimated budget

--- saba/data/utils.ts
+++ saba/data/utils.ts
@@ -1,4 +1,6 @@
+import { BaseItem, BaseKeystoneTypeInfo, BaseListTypeInfo, KeystoneContext, MaybeItemFunction, MaybeSessionFunction } from "@keystone-6/core/types";
 import axios from "axios";
+import { Roles, Session } from "./types";
 
 export async function SendMessageToTelegram(
   message: string,
@@ -29,4 +31,40 @@ export namespace NumUtils {
   export function deformat(number: string) {
     return BigInt(number.replace(/,/g, ''));
   }
+}
+
+
+export function setPermitions(
+  args: {
+    context: KeystoneContext<BaseKeystoneTypeInfo<any>>;
+    session?: Session;
+    item: BaseItem;
+  },
+
+  permittions: {
+    role: Roles,
+    fieldMode: "read" | "edit" | "hidden"
+  }[],
+  defaultValue: "read" | "edit" | "hidden"
+
+): "read" | "edit" | "hidden" {
+
+  for (const per of permittions) {
+    if (per.role === args.session?.data.role) {
+      return per.fieldMode
+    }
+  }
+
+  return defaultValue
+
+}
+
+export function editIfAdmin(args: {
+  context: KeystoneContext<BaseKeystoneTypeInfo<any>>;
+  session?: Session;
+  item: BaseItem;
+},
+  defaultValue?: "read" | "edit" | "hidden"
+) {
+  return setPermitions(args, [{ role: Roles.admin, fieldMode: 'edit' }], defaultValue || 'read')
 }
