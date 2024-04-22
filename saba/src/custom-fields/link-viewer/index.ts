2f98e62c9f96351a879fc4a73e30ab26fcb3a00f
mahdiyar anari <mahdiyaranari@yahoo.com>
2024-04-22 15:40:35 +0330

    Design

---
+++ saba/src/custom-fields/link-viewer/index.ts
@@ -0,0 +1,73 @@
+import {
+    BaseListTypeInfo,
+    fieldType,
+    FieldTypeFunc,
+    CommonFieldConfig,
+    orderDirectionEnum,
+} from '@keystone-6/core/types';
+import { graphql } from '@keystone-6/core';
+
+
+export type UtilsBarListTypeInfo = {
+    email?: string | null,
+    phone?: string | null,
+    full_name?: string | null
+}
+
+export type SendResetPasswordEmailConfig<ListTypeInfo extends BaseListTypeInfo> =
+    CommonFieldConfig<ListTypeInfo> & UtilsBarListTypeInfo
+
+
+
+export const sendResetPasswordEmail =
+    <ListTypeInfo extends BaseListTypeInfo>({
+
+        email = null,
+        phone = null,
+        full_name = null,
+        ...config
+    }: SendResetPasswordEmailConfig<ListTypeInfo> = {}): FieldTypeFunc<ListTypeInfo> =>
+        (meta) => {
+
+
+            return fieldType({
+                kind: 'none',
+            })({
+                // this passes through all of the common configuration like access control and etc.
+                ...config,
+                // hooks: {
+                //     ...config.hooks,
+                //     // We use the `validateInput` hook to ensure that the user doesn't set an out of range value.
+                //     // This hook is the key difference on the backend between the stars field type and the integer field type.
+                //     async validateInput(args) {
+                //         const val = args.resolvedData[meta.fieldKey] as
+                //             | string
+                //             | undefined;
+                //         if (args.operation === 'create')
+                //             if (val === '' || val === undefined) {
+                //                 args.addValidationError(
+                //                     `The value must be within the range of 0-${utils}`
+                //                 );
+                //             }
+
+                //         await config.hooks?.validateInput?.(args);
+                //     },
+                // },
+
+                output: graphql.field({
+                    type: graphql.JSON, // returns an object
+                    resolve(args) {
+                        return args.item.id
+
+                    },
+
+                }),
+                views: './src/custom-fields/link-viewer/views',
+                getAdminMeta() {
+                    return {
+                        email,
+                        phone
+                    };
+                },
+            });
+        }
---
+++ saba/src/custom-fields/link-viewer/link-viewer.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { useTheme } from '@keystone-ui/core';
+import { Button } from '@keystone-ui/button';
+import { useToasts } from "@keystone-ui/toast"
+// import { gql, useMutation } from '@keystone-6/core/admin-ui/apollo';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  useQuery
+} from '@apollo/client'
+
+type UtilsBarProps = {
+  value: string | null;
+  onChange?: (value: string | null) => void;
+};
+
+
+
+export function UtilsBar(props: UtilsBarProps) {
+
+
+  const [emailIsSent, setEmailIsSent] = useState(false);
+  const { addToast } = useToasts()
+
+
+  const { data } = useQuery(gql`
+        query DesignFiles($where: DesignWhereUniqueInput!) {
+          design(where: $where) {
+            id
+            design {
+              file {
+                filename
+                url
+              }
+            }
+          }
+        }
+      `,
+    {
+      variables: {
+        "where": {
+          "id": props.value
+        }
+      }
+    }
+  )
+
+
+  return (
+    <div>
+      {/* 
+      <pre>
+        {JSON.stringify(data?.design.design || {}, null, 2)}
+      </pre> */}
+      {
+        data?.design.design.map((d) => (
+          <div>
+
+            <a href={d.file.url} download={d.file.filename} target="_blank" rel="noreferrer">
+              {d.file.filename}
+            </a>
+          </div>
+        ))
+      }
+
+    </div>
+  );
+}
