f635cf75ac7723ad53bea046bd121a31b0d24d16
mahdiyar <a.mahdiyar7@live.com>
2024-04-26 10:58:45 +0330

    dashboard is ready

---
+++ saba/admin/components/CustomNavigation.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavigationContainer, ListNavItems, NavItem } from '@keystone-6/core/admin-ui/components';
+import type { NavigationProps } from '@keystone-6/core/admin-ui/components';
+
+export function CustomNavigation({ lists, authenticatedItem }: NavigationProps) {
+
+    return (
+        <NavigationContainer authenticatedItem={authenticatedItem}>
+            <NavItem href="/">Dashboard</NavItem>
+            <ListNavItems lists={lists} include={['Approval', 'Design']} />
+        </NavigationContainer>
+    )
+}
--- saba/admin/config.tsx
+++ saba/admin/config.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import type { AdminConfig } from '@keystone-6/core/types';
+import { CustomNavigation } from './components/CustomNavigation';
 
 function CustomLogo() {
     return <h3>پنل کنترل پروژه صباپیشرو </h3>
 }
 
-export const components = {
-    Logo: CustomLogo
-}
+export const components: AdminConfig['components'] = {
+    Logo: CustomLogo,
+    Navigation: CustomNavigation
+};
