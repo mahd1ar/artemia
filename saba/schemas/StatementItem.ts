bf980d70aeb2ab3a769a05b35650aa39d52319d0
mahdiyar <a.mahdiyar7@live.com>
2024-05-08 13:14:47 +0330

    remove all console.logs

--- saba/schemas/StatementItem.ts
+++ saba/schemas/StatementItem.ts
@@ -12,13 +12,6 @@ export const StatementItem = list({
       return !((args.session as Session)?.data.role === Roles.admin)
     },
   },
-  hooks: {
-    validate(args) {
-      console.log(args.item)
-      console.log(args.inputData)
-      console.log(args.resolvedData)
-    },
-  },
   fields: {
     description: text(),
     unit: select({
