de71580b669019edf806f6c9bc498a9bf8986f11
mahdiyar <a.mahdiyar7@live.com>
2024-05-30 12:07:14 +0330

    add: technikal group

--- saba/schemas/User.ts
+++ saba/schemas/User.ts
@@ -1,22 +1,54 @@
-import { list, graphql } from "@keystone-6/core";
+import { list, graphql, group } from "@keystone-6/core";
 import { allowAll, allOperations } from "@keystone-6/core/access";
 import { password, relationship, select, text, timestamp, virtual } from "@keystone-6/core/fields";
 import { Roles, Session, getRoleFromArgs } from "../data/types";
+import type { Lists } from ".keystone/types";
 
-export const User = list({
+
+const ui = { itemView: { fieldMode(args: any) { return getRoleFromArgs(args) > Roles.operator ? 'hidden' : 'edit' } } }
+
+export const User = list<Lists.User.TypeInfo<any>>({
   access: {
     // operation: allOperations(
     //   isAdmin
     // ),
-    operation: allowAll,
-  },
-  ui: {
-    isHidden(args) {
-      return getRoleFromArgs(args) > Roles.operator
+    operation: {
+      create: args => getRoleFromArgs(args) <= Roles.operator,
+      delete: args => getRoleFromArgs(args) <= Roles.operator,
+      query: () => true,
+      update: () => true
     },
+    filter: {
+      query: args => {
+        // if (process.env.NODE_ENV !== 'production')
+        //   return true
+
+        if (getRoleFromArgs(args) <= Roles.operator)
+          return true
+
+        else {
+          return {
+            id: {
+              equals: (args.session as Session)?.itemId
+            }
+          }
+        }
+      },
+    }
+  },
+
+  ui: {
+    label: 'کاربر',
+    // isHidden(args) {
+    //   return getRoleFromArgs(args) > Roles.operator
+    // },
     listView: {
       initialColumns: ['name', 'role']
-    }
+    },
+
+    hideCreate(args) {
+      return getRoleFromArgs(args) > Roles.operator
+    },
   },
   fields: {
     fullname: virtual({
@@ -33,6 +65,13 @@ export const User = list({
       isIndexed: "unique",
     }),
     role: select({
+      ui: {
+        itemView: {
+          fieldMode(args) {
+            return process.env.NODE_ENV !== 'production' ? 'edit' : getRoleFromArgs(args) > Roles.operator ? 'read' : 'edit'
+          },
+        }
+      },
       options: [
         {
           label: " مدیر کل",
@@ -63,13 +102,25 @@ export const User = list({
       defaultValue: Roles.guest,
     }),
     password: password({ validation: { isRequired: true } }),
-    statements: relationship({ ref: "Statement.createdBy", many: true }),
-    approvals: relationship({ ref: "Approval.createdBy", many: true }),
-    descriptions: relationship({ ref: "Description.createdBy", many: true }),
-    Designs: relationship({ ref: "Design.createdBy", many: true }),
-    dailyReports: relationship({ ref: "DailyReport.createdBy", many: true }),
+    ...group({
+      label: 'data',
+      fields: {
+
+        statements: relationship({ ui, ref: "Statement.createdBy", many: true }),
+        approvals: relationship({ ui, ref: "Approval.createdBy", many: true }),
+        descriptions: relationship({ ui, ref: "Description.createdBy", many: true }),
+        Designs: relationship({ ui, ref: "Design.createdBy", many: true }),
+        dailyReports: relationship({ ui, ref: "DailyReport.createdBy", many: true }),
+      }
+    }),
 
     createdAt: timestamp({
+      ui: {
+        itemView: {
+          fieldPosition: 'sidebar',
+          fieldMode: 'read'
+        }
+      },
       defaultValue: { kind: "now" },
     }),
   },
