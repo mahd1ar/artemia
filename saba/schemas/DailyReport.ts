963668eb7907fb583bfb06a73989f55eb7022862
mahdiyar <a.mahdiyar7@live.com>
2024-06-22 15:02:34 +0330

    fix daily reports date in persion

--- saba/schemas/DailyReport.ts
+++ saba/schemas/DailyReport.ts
@@ -1,100 +1,113 @@
 import { list } from "@keystone-6/core";
 import { allOperations } from "@keystone-6/core/access";
-import { file, relationship, select, text, timestamp } from "@keystone-6/core/fields";
+import {
+    file,
+    relationship,
+    select,
+    text,
+    timestamp,
+} from "@keystone-6/core/fields";
 import { isAdmin, isLoggedIn } from "../data/access";
-import { Session, Roles } from "../data/types"
+import { Session, Roles, getRoleFromArgs } from "../data/types";
 import { setPermitions } from "../data/utils";
 
 export const DailyReport = list({
     access: {
-        operation: allOperations(isLoggedIn)
+        operation: {
+            create: (args) =>
+                [Roles.admin, Roles.operator].includes(getRoleFromArgs(args)),
+            delete: (args) =>
+                [Roles.admin, Roles.operator].includes(getRoleFromArgs(args)),
+            update: (args) =>
+                [Roles.admin, Roles.operator, Roles.workshop].includes(
+                    getRoleFromArgs(args)
+                ),
+            query: () => true,
+        },
     },
     ui: {
         listView: {
-            initialColumns: ['date'],
+            initialColumns: ["date"],
             initialSort: {
-                field: 'date',
-                direction: 'DESC',
+                field: "date",
+                direction: "DESC",
             },
         },
-        label: 'گزارش روزانه',
+        label: "گزارش روزانه",
         hideCreate(args) {
-            const session = args.context.session as Session
-            return !(session?.data.role === Roles.admin)
+            return ![Roles.admin, Roles.operator].includes(getRoleFromArgs(args));
         },
     },
     hooks: {
         validate(args) {
-            const session = args.context.session as Session
-            if (args.operation === 'update') {
+            const session = args.context.session as Session;
+            if (args.operation === "update") {
                 if (session?.data.role === Roles.workshop) {
-                    const createdAt = args.item.date as Date
+                    const createdAt = args.item.date as Date;
                     // check if user is not exceeded 48 hours
-                    const diff = (Date.now() - createdAt.getTime()) / (1000 * 60 * 60)
+                    const diff = (Date.now() - createdAt.getTime()) / (1000 * 60 * 60);
 
                     if (diff > 48) {
-                        args.addValidationError('48 ساعت از مهلت بارگذاری این گزارش گذشته است')
+                        args.addValidationError(
+                            "48 ساعت از مهلت بارگذاری این گزارش گذشته است"
+                        );
                     }
                 }
-            }
-
-            if (args.operation === 'delete') {
-
-                const session = args.context.session as Session
-                if (session?.data.role !== Roles.admin) {
-                    args.addValidationError('شما دسترسی لازم برای حذف این گزارش را ندارید')
-                }
-
             }
         },
     },
     fields: {
+        date: timestamp({
+            label: 'تاریخ',
+            defaultValue: { kind: "now" },
+            ui: {
+                views: "./src/custome-fields-view/date-persian-cell.tsx",
+                createView: { fieldMode: "hidden" },
+                itemView: {
+                    fieldMode(args) {
+                        return "read";
+                    },
+                },
+            },
+        }),
         document: file({
-            storage: 'file'
+            label: 'فایل گزارش روز',
+            storage: "file",
         }),
         createdBy: relationship({
-            ref: 'User.dailyReports',
+            ref: "User.dailyReports",
             many: false,
             ui: {
                 createView: { fieldMode: "hidden" },
                 itemView: {
-                    fieldPosition: 'sidebar',
+                    fieldPosition: "sidebar",
                     fieldMode(args) {
-
-                        return setPermitions(args, [{ role: Roles.admin, fieldMode: 'edit' }], 'read')
+                        return setPermitions(
+                            args,
+                            [{ role: Roles.admin, fieldMode: "edit" }],
+                            "read"
+                        );
                     },
-                }
+                },
             },
             hooks: {
                 resolveInput(args) {
-                    const session = args.context.session as Session
+                    const session = args.context.session as Session;
 
-                    if (args.inputData.createdBy && session?.data.role !== Roles.workshop) {
-                        return args.resolvedData.createdBy
+                    if (
+                        args.inputData.createdBy &&
+                        session?.data.role !== Roles.workshop
+                    ) {
+                        return args.resolvedData.createdBy;
                     }
 
-                    if (args.operation === 'update' && !args.item.createdBy) {
-
-                        args.resolvedData.createdBy = { connect: { id: session?.itemId } }
+                    if (args.operation === "update" && !args.item.createdBy) {
+                        args.resolvedData.createdBy = { connect: { id: session?.itemId } };
                     }
 
-                    return args.resolvedData.createdBy
-                }
-            }
+                    return args.resolvedData.createdBy;
+                },
+            },
         }),
-        date: timestamp(
-            {
-                defaultValue: { kind: "now" },
-                ui: {
-                    views: './src/custome-fields-view/date-persian-cell.tsx',
-                    createView: { fieldMode: "hidden" },
-                    itemView: {
-                        fieldPosition: 'sidebar',
-                        fieldMode(args) {
-                            return 'read'
-                        },
-                    }
-                }
-            }),
     },
-})
+});
