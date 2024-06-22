ab7facceefe678d6aba6979e5fcc8eaf28ecc0f2
mahdiyar <a.mahdiyar7@live.com>
2024-06-22 15:02:34 +0330

    fix daily reports date in persion

--- saba/src/custome-fields-view/date-persian-cell.tsx
+++ saba/src/custome-fields-view/date-persian-cell.tsx
@@ -1,8 +1,41 @@
 import React from "react";
-import { CellComponent } from "@keystone-6/core/types";
+import { CellComponent, FieldProps } from "@keystone-6/core/types";
 import { CellContainer, CellLink } from "@keystone-6/core/admin-ui/components";
+import { controller } from "@keystone-6/core/fields/types/timestamp/views";
+import { FieldContainer, FieldLabel } from "@keystone-ui/fields";
 
 
+export const Field = ({
+  field,
+  value,
+  onChange,
+  autoFocus,
+  itemValue,
+  forceValidation
+}: FieldProps<typeof controller>) => {
+
+  console.log(field)
+
+  return (
+    <FieldContainer>
+
+      <FieldLabel>{field.label}</FieldLabel>
+      {onChange ? (
+        <input type="date" />
+      ) : value.value === null ? (
+        ' 0'
+      ) : (
+        <span dir='rtl' >
+          {
+            // @ts-ignore
+            new Date(value.value.dateValue).toLocaleDateString("fa-IR", { dateStyle: 'full' })
+          }
+        </span>
+      )}
+    </FieldContainer>
+  )
+
+};
 
 export const Cell: CellComponent = ({ item, field, linkTo }) => {
   let value = item[field.path]
