51f7b6cc493a3ab7d3e1aba1e7deff75a23dd676
mahdiyar <a.mahdiyar7@yahoo.com>
2024-07-31 17:05:40 +0330

    feat: Simplify access control logic, add approval features and improve permissions.

--- saba/data/access.ts
+++ saba/data/access.ts
@@ -1,8 +1,6 @@
-import { BaseAccessArgs } from "@keystone-6/core/dist/declarations/src/types/config/access-control";
-import { Roles, Session } from "./types";
-import { BaseListTypeInfo } from "@keystone-6/core/types";
+import { getRoleFromArgs, Roles, Session } from "./types";
 
-export function isLoggedIn(args: BaseAccessArgs<BaseListTypeInfo>) {
+export function isLoggedIn(args: { session?: Session } & Record<string, any>) {
   const { session }: Partial<{ session: Session }> = args;
 
   if (!!session === false) return false;
@@ -10,11 +8,16 @@ export function isLoggedIn(args: BaseAccessArgs<BaseListTypeInfo>) {
   return !!session;
 }
 
-export function isAdmin(args: BaseAccessArgs<BaseListTypeInfo>) {
+export function isAdmin(args: { session?: Session } & Record<string, any>) {
   return isLoggedIn(args) && args.context.session!.data.role === Roles.admin;
 }
 
 export function isMobayen(args: { session?: Session } & Record<string, any>) {
   const role = (args.session as Session)!.data.role
   return role === Roles.workshop
+}
+
+export function isMemberOfAdminGroup(args: Record<string, any> & { session?: Session }) {
+
+  return (getRoleFromArgs(args, Roles.guest) <= Roles.operator)
 }
