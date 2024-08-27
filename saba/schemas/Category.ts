50ac9f1f9cbe742974c7fb7838c2e30298ea73a2
mahdiyar <a.mahdiyar7@live.com>
2024-08-27 15:51:36 +0330

    fix category caode on change parent parent

--- saba/schemas/Category.ts
+++ saba/schemas/Category.ts
@@ -1,9 +1,10 @@
 import { list } from "@keystone-6/core";
-import { allowAll } from "@keystone-6/core/access";
+import { allOperations, allowAll } from "@keystone-6/core/access";
 import { relationship, select, text, timestamp } from "@keystone-6/core/fields";
 import { Roles, Session } from "../data/types";
 import { setPermitions } from "../data/utils";
 import type { Lists } from ".keystone/types";
+import { isLoggedIn } from "../data/access";
 
 
 function findMissingNumber(arr: number[]) {
@@ -20,49 +21,77 @@ function findMissingNumber(arr: number[]) {
     return false; // Sequence is complete
 }
 
+async function generateCode(itemId: string | null, parentId: string | null, prisma: Lists.Category.TypeInfo<Session>['all']['prisma']) {
+
+
+    console.log({ parentId })
+    let siblings = await prisma.category.findMany({
+        where: {
+            parent: parentId ? {
+                id: {
+                    equals: parentId
+                }
+            } : null,
+        },
+        select: {
+            parent: {
+                select: {
+                    id: true,
+                    code: true
+                },
+            },
+            code: true,
+            id: true
+        }
+    })
+
+    if (itemId)
+        siblings = siblings.filter(child => child.id !== itemId)
+
+    let parentCode: string | null = null
+
+    if (parentId) {
+
+        const parentCategory = await prisma.category.findUnique({
+            where: {
+                id: parentId
+            },
+            select: {
+                code: true
+            }
+        })
+
+        parentCode = parentCategory?.code || null
+    }
+
+
+
+
+    let numbers: number[]
+
+    if (parentCode) {
+        const regex = new RegExp(`^${parentCode}`);
+        numbers = siblings.map(child => +(child.code.replace(regex, ''))).filter(Boolean)
+    } else {
+        numbers = siblings.map(child => +(child.code)).filter(Boolean)
+    }
+
+    if (numbers.length) {
+        const missingNumber = findMissingNumber(numbers.sort())
+        if (missingNumber) return `${parentCode || ''}${missingNumber > 9 ? missingNumber : `0${missingNumber}`}`
+        else {
+            const code = Math.max(...numbers) + 1
+
+            return `${parentCode || ''}${code > 9 ? code : `0${code}`}`
+        }
+    } else return `${parentCode || ''}01`
+}
+
 export const Category = list<Lists.Category.TypeInfo<Session>>({
     access: {
-        operation: allowAll,
-        // filter: {
-        //     query: async args => {
-        //         const referer = new URL(args.context.req!.headers.referer || '')
-
-        //         return referer.pathname === '/categories' && !referer.search ?
-        //             {
-        //                 parent: null
-        //             }
-        //             : true
-        //     }
-        // }
-        // filter: {
-        //     query: async (args) => {
-        //         const referer = args.context.req?.headers.referer;
-        //         if (!referer) return true;
-
-        //         const from = new URL(referer).pathname.split("/").filter(Boolean).at(0);
-
-        //         if (from === "categories") return true;
-
-        //         if (from === "designs") {
-
-        //             const prisma = args.context.prisma as PrismaClient;
-        //             const setting = await prisma.setting.findFirst();
-        //             const parrentCategory = setting?.parentCategoryOfDesignTag;
-
-        //             if (!parrentCategory) return true;
-
-        //             return {
-        //                 parent: {
-        //                     id: {
-        //                         equals: parrentCategory,
-        //                     },
-        //                 },
-        //             };
-        //         }
-
-        //         return true;
-        //     },
-        // },
+        operation: {
+            ...allOperations(isLoggedIn)
+        },
     },
     hooks: {
 
@@ -89,7 +118,89 @@ export const Category = list<Lists.Category.TypeInfo<Session>>({
                     where: children
                 })
             }
-        }
+        },
+        async afterOperation(args) {
+
+
+            if (args.operation === 'update' && (args.inputData.parent?.connect?.id || args.inputData.parent?.disconnect)) {
+                console.log("aslan ejra mishe")
+
+                const oldCode = args.originalItem.code
+                const newCode = args.item.code
+                const itemId = args.item.id
+
+                if (oldCode === newCode) return
+
+                const allChildren = await args.context.prisma.category.findMany({
+                    where: {
+                        AND: [
+                            {
+                                code: {
+                                    startsWith: oldCode
+                                }
+                            },
+                            {
+                                code: {
+                                    not: {
+                                        equals: oldCode
+                                    }
+                                }
+                            }
+                        ]
+                    },
+                    select: {
+                        code: true,
+                        id: true,
+                        title: true,
+                        parentId: true,
+                    }
+                })
+
+                allChildren.sort((a, b) => +a.code - +b.code)
+                console.log("allChildren")
+                console.log(allChildren)
+                if (allChildren.length === 0) return
+
+
+
+                let range = {
+                    from: 0,
+                    to: allChildren.length,
+
+                    [Symbol.asyncIterator]() { // (1)
+                        return {
+                            current: this.from,
+                            last: this.to,
+
+                            async next() { // (2)
+
+                                if (this.current < this.last) {
+                                    const ch = allChildren[this.current]
+                                    const data = await args.context.query.Category.updateOne({
+                                        where: {
+                                            id: ch.id
+                                        },
+                                        data: {
+                                            code: ''
+                                        },
+                                        query: 'code'
+                                    })
+                                    this.current++
+                                    return { done: false, value: data.code };
+                                } else {
+                                    return { done: true };
+                                }
+                            }
+                        };
+                    }
+                };
+
+                for await (const _ of range) {
+                }
+
+
+            }
+        },
     },
     ui: {
         itemView: {
@@ -102,122 +213,37 @@ export const Category = list<Lists.Category.TypeInfo<Session>>({
             },
         },
     },
+
     fields: {
         code: text({
             hooks: {
                 async resolveInput(args) {
+                    // wether it is update or create
+                    if (!args.inputData.code) {
 
-                    if (args.operation === 'create') {
-                        if (!args.inputData.code) {
-                            const parentId = args.inputData.parent?.connect?.id ||
-                                args.resolvedData.parent?.connect?.id ||
-                                args.resolvedData.parent?.create?.id
+                        const parentId = args.inputData.parent?.connect?.id ||
+                            args.resolvedData.parent?.connect?.id ||
+                            args.resolvedData.parent?.create?.id || args.item?.parentId || null
 
-                            if (parentId) {
-                                const parentCategory = await args.context.prisma.category.findUnique({
-                                    where: {
-                                        id: parentId
-                                    },
-                                    select: {
-                                        code: true,
-                                        children: {
-                                            select: {
-                                                id: true,
-                                                code: true
-                                            }
-                                        }
-                                    }
-                                })
 
-                                if (parentCategory) {
-                                    const regex = new RegExp(`^${parentCategory.code}`);
+                        return await generateCode(args.item?.id || null, parentId, args.context.prisma)
 
-                                    const numbers = parentCategory.children.map(child => +(child.code.replace(regex, ''))).filter(Boolean)
-                                    if (numbers.length) {
-                                        const missingNumber = findMissingNumber(numbers.sort())
-                                        if (missingNumber) return `${parentCategory.code}${missingNumber > 9 ? missingNumber : `0${missingNumber}`}`
-                                        else {
-                                            const code = Math.max(...numbers) + 1
 
-                                            return `${parentCategory.code}${code > 9 ? code : `0${code}`}`
-                                        }
-                                    } else return `${parentCategory.code}01`
 
-                                }
-                            }
-
-                        }
 
 
                     }
 
-                    if (args.operation === 'update' && args.inputData.parent?.connect?.id) {
+                    if (args.operation === 'update' && (args.inputData.parent?.connect?.id || args.inputData.parent?.disconnect)) {
 
-                        const parentId = args.inputData.parent.connect.id
+                        const parentId = args.inputData.parent?.connect?.id || null
 
 
-                        const parentCategory = await args.context.prisma.category.findUnique({
-                            where: {
-                                id: parentId
-                            },
-                            select: {
-                                code: true,
-                                children: {
-                                    select: {
-                                        id: true,
-                                        code: true
-                                    }
-                                }
-                            }
-                        })
-
-                        if (parentCategory) {
-                            const regex = new RegExp(`^${parentCategory.code}`);
-
-                            const numbers = parentCategory.children.map(child => +(child.code.replace(regex, ''))).filter(Boolean)
-                            if (numbers.length) {
-                                const missingNumber = findMissingNumber(numbers.sort())
-                                if (missingNumber) return `${parentCategory.code}${missingNumber > 9 ? missingNumber : `0${missingNumber}`}`
-                                else {
-                                    const code = Math.max(...numbers) + 1
-
-                                    return `${parentCategory.code}${code > 9 ? code : `0${code}`}`
-                                }
-                            } else return `${parentCategory.code}01`
-
-                        }
+                        return await generateCode(args.item.id, parentId, args.context.prisma)
 
                     }
 
-                    // TODO test this
-                    if (args.operation === 'update' && args.inputData.parent?.disconnect) {
 
-                        const siblings = await args.context.prisma.category.findMany({
-                            where: {
-                                parent: null
-                            },
-                            select: {
-                                code: true,
-                            }
-                        })
-
-                        if (siblings) {
-
-                            const numbers = siblings.map(child => +child.code).filter(Boolean)
-
-                            if (numbers.length) {
-                                const missingNumber = findMissingNumber(numbers.sort())
-                                if (missingNumber) return `${missingNumber > 9 ? missingNumber : `0${missingNumber}`}`
-                                else {
-                                    const code = Math.max(...numbers) + 1
-
-                                    return `${code > 9 ? code : `0${code}`}`
-                                }
-                            } else return `01`
-
-                        }
-
-                    }
 
                     return args.resolvedData.code
                 },
@@ -244,38 +270,6 @@ export const Category = list<Lists.Category.TypeInfo<Session>>({
                 }
             },
             ref: "Category.children",
-            hooks: {
-                // async resolveInput(args) {
-                //     console.log(args.resolvedData);
-                //     if (args.operation === "create") {
-                //         const referer = args.context.req?.headers.referer;
-                //         if (referer) {
-                //             const from = new URL(referer).pathname
-                //                 .split("/")
-                //                 .filter(Boolean)
-                //                 .at(0);
-                //             if (from === "categories") {
-                //                 return args.resolvedData.parent;
-                //             } else {
-                //                 const prisma = args.context.prisma as PrismaClient;
-                //                 const settings = await prisma.setting.findFirst();
-
-                //                 if (from === "designs") {
-                //                     if (settings?.parentCategoryOfDesignTag) {
-                //                         console.log(99090);
-                //                         console.log(settings?.parentCategoryOfDesignTag);
-                //                         return {
-                //                             connect: { id: settings.parentCategoryOfDesignTag },
-                //                         };
-                //                     }
-                //                 }
-                //             }
-                //         }
-                //     }
-
-                //     return args.resolvedData.parent;
-                // },
-            },
         }),
 
         designs: relationship({
