e54f3c2b016e940fc254b6f74dd41f77b9ef1d50
mahdiyar <a.mahdiyar7@yahoo.com>
2025-01-24 18:18:43 +0330

    feat: add sharp

--- saba/schemas/FileStore.ts
+++ saba/schemas/FileStore.ts
@@ -1,10 +1,54 @@
 import type { Lists } from '.keystone/types'
 import type { Session } from '../data/types'
+import fs from 'node:fs'
+import path from 'node:path'
 import { list } from '@keystone-6/core'
 import { allOperations, allowAll } from '@keystone-6/core/access'
 import { file, relationship, select, text, timestamp } from '@keystone-6/core/fields'
+import sharp from 'sharp'
 import { getRoleFromArgs, Roles } from '../data/types'
 import { editIfAdmin } from '../data/utils'
+
+async function resizeImageIfNecessary(srcDirectory: string, inputFilenameWithExtention: string): Promise<{ filepath: string, filesize: number } | boolean> {
+  const inputPath = path.join(srcDirectory, inputFilenameWithExtention)
+
+  const outputFileNameArray = inputPath.split('.')
+  outputFileNameArray[outputFileNameArray.length - 2] = `${outputFileNameArray.at(-2)}-resized`
+  const outputFileName = outputFileNameArray.join('.')
+
+  try {
+    sharp.cache(false)
+    const image = sharp(inputPath)
+    const metadata = await image.metadata()
+
+    if (!metadata.width || !metadata.height) {
+      return false
+    }
+    // Check if the width is greater than 1000 pixels
+    if (metadata.width > 1200) {
+      const { size: filesize } = await image.resize({ width: 1000 }).toFile(outputFileName)
+
+      return {
+        filepath: outputFileName,
+        filesize,
+      }
+    }
+
+    else if (metadata.height > 1200) {
+      const { size: filesize } = await image.resize({ height: 1000 }).toFile(outputFileName)
+      return {
+        filepath: outputFileName,
+        filesize,
+      }
+    }
+
+    return true
+  }
+  catch (error) {
+    console.error(`Error processing ${inputPath}:`, error)
+    return false
+  }
+}
 
 export const FileStore = list<Lists.FileStore.TypeInfo<Session>>({
   access: allowAll,
@@ -20,12 +64,47 @@ export const FileStore = list<Lists.FileStore.TypeInfo<Session>>({
     },
   },
   hooks: {
-    resolveInput(args) {
+    async resolveInput(args) {
       if (args.operation === 'create' && !args.inputData.title) {
         args.resolvedData.title = args.resolvedData.file.filename || ''
       }
 
       return args.resolvedData
+    },
+    async afterOperation(args) {
+      if (args.operation === 'delete')
+        return
+
+      const prisma = args.context.prisma
+
+      const filename = args.resolvedData.file.filename?.toString()
+
+      if (filename) {
+        const absDir = path.join(
+          process.cwd(),
+          `/public/files/`,
+        )
+
+        const res = await resizeImageIfNecessary(absDir, filename)
+
+        if (typeof res === 'boolean') {
+          return
+        }
+
+        if (res) {
+          const srcFilePath = path.join(absDir, filename)
+
+          fs.unlinkSync(srcFilePath)
+          fs.renameSync(res.filepath, srcFilePath)
+
+          await prisma.fileStore.update({
+            where: { id: args.item.id },
+            data: {
+              file_filesize: res.filesize,
+            },
+          })
+        }
+      }
     },
   },
   fields: {
